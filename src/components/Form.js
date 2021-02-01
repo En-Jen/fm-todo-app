b129a8057e9872bfb07f10752d08832d1ab7a971
Jen Speak <jspeak6@gmail.com>
2021-01-31 23:05:29 -0800

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Form = ({ inputText, setInputText, todos, setTodos }) => {
+	const inputTextHandler = e => {
+		setInputText(e.target.value);
+	};
+
+	const submitTodoHandler = e => {
+		if (e.key === 'Enter') {
+			e.preventDefault();
+
+			setTodos([
+				...todos,
+				{ text: inputText, completed: false, id: Math.random() * 1000 },
+			]);
+			setInputText('');
+		}
+	};
+
+	return (
+		<form>
+			<input
+				onKeyDown={submitTodoHandler}
+				onChange={inputTextHandler}
+				value={inputText}
+				type="text"
+			/>
+		</form>
+	);
+};
+
+export default Form;
